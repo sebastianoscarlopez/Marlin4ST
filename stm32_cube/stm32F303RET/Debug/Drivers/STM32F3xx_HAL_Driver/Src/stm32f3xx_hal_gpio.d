d86fb6856e308bb4b6d8fb5b01e1c890628d3c7b
sebastianoscarlopez <sebastianoscarlopez@gmail.com>
2017-06-25 15:32:12 -0300

    saved work

---
+++ stm32_cube/stm32F303RET/Debug/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_gpio.d
@@ -0,0 +1,80 @@
+Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_gpio.o: \
+ ../Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_gpio.c \
+ D:/git/Marlin4ST/stm32_cube/stm32F303RET/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal.h \
+ D:/git/Marlin4ST/stm32_cube/stm32F303RET/Inc/stm32f3xx_hal_conf.h \
+ D:/git/Marlin4ST/stm32_cube/stm32F303RET/Inc/main.h \
+ D:/git/Marlin4ST/stm32_cube/stm32F303RET/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_rcc.h \
+ D:/git/Marlin4ST/stm32_cube/stm32F303RET/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_def.h \
+ D:/git/Marlin4ST/stm32_cube/stm32F303RET/Drivers/CMSIS/Device/ST/STM32F3xx/Include/stm32f3xx.h \
+ D:/git/Marlin4ST/stm32_cube/stm32F303RET/Drivers/CMSIS/Device/ST/STM32F3xx/Include/stm32f303xe.h \
+ D:/git/Marlin4ST/stm32_cube/stm32F303RET/Drivers/CMSIS/Include/core_cm4.h \
+ D:/git/Marlin4ST/stm32_cube/stm32F303RET/Drivers/CMSIS/Include/core_cmInstr.h \
+ D:/git/Marlin4ST/stm32_cube/stm32F303RET/Drivers/CMSIS/Include/cmsis_gcc.h \
+ D:/git/Marlin4ST/stm32_cube/stm32F303RET/Drivers/CMSIS/Include/core_cmFunc.h \
+ D:/git/Marlin4ST/stm32_cube/stm32F303RET/Drivers/CMSIS/Include/core_cmSimd.h \
+ D:/git/Marlin4ST/stm32_cube/stm32F303RET/Drivers/CMSIS/Device/ST/STM32F3xx/Include/system_stm32f3xx.h \
+ D:/git/Marlin4ST/stm32_cube/stm32F303RET/Drivers/STM32F3xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ D:/git/Marlin4ST/stm32_cube/stm32F303RET/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_rcc_ex.h \
+ D:/git/Marlin4ST/stm32_cube/stm32F303RET/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_gpio.h \
+ D:/git/Marlin4ST/stm32_cube/stm32F303RET/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_gpio_ex.h \
+ D:/git/Marlin4ST/stm32_cube/stm32F303RET/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_dma.h \
+ D:/git/Marlin4ST/stm32_cube/stm32F303RET/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_dma_ex.h \
+ D:/git/Marlin4ST/stm32_cube/stm32F303RET/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_cortex.h \
+ D:/git/Marlin4ST/stm32_cube/stm32F303RET/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_flash.h \
+ D:/git/Marlin4ST/stm32_cube/stm32F303RET/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_flash_ex.h \
+ D:/git/Marlin4ST/stm32_cube/stm32F303RET/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_i2c.h \
+ D:/git/Marlin4ST/stm32_cube/stm32F303RET/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_i2c_ex.h \
+ D:/git/Marlin4ST/stm32_cube/stm32F303RET/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_pwr.h \
+ D:/git/Marlin4ST/stm32_cube/stm32F303RET/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_pwr_ex.h
+
+D:/git/Marlin4ST/stm32_cube/stm32F303RET/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal.h:
+
+D:/git/Marlin4ST/stm32_cube/stm32F303RET/Inc/stm32f3xx_hal_conf.h:
+
+D:/git/Marlin4ST/stm32_cube/stm32F303RET/Inc/main.h:
+
+D:/git/Marlin4ST/stm32_cube/stm32F303RET/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_rcc.h:
+
+D:/git/Marlin4ST/stm32_cube/stm32F303RET/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_def.h:
+
+D:/git/Marlin4ST/stm32_cube/stm32F303RET/Drivers/CMSIS/Device/ST/STM32F3xx/Include/stm32f3xx.h:
+
+D:/git/Marlin4ST/stm32_cube/stm32F303RET/Drivers/CMSIS/Device/ST/STM32F3xx/Include/stm32f303xe.h:
+
+D:/git/Marlin4ST/stm32_cube/stm32F303RET/Drivers/CMSIS/Include/core_cm4.h:
+
+D:/git/Marlin4ST/stm32_cube/stm32F303RET/Drivers/CMSIS/Include/core_cmInstr.h:
+
+D:/git/Marlin4ST/stm32_cube/stm32F303RET/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+D:/git/Marlin4ST/stm32_cube/stm32F303RET/Drivers/CMSIS/Include/core_cmFunc.h:
+
+D:/git/Marlin4ST/stm32_cube/stm32F303RET/Drivers/CMSIS/Include/core_cmSimd.h:
+
+D:/git/Marlin4ST/stm32_cube/stm32F303RET/Drivers/CMSIS/Device/ST/STM32F3xx/Include/system_stm32f3xx.h:
+
+D:/git/Marlin4ST/stm32_cube/stm32F303RET/Drivers/STM32F3xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+D:/git/Marlin4ST/stm32_cube/stm32F303RET/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_rcc_ex.h:
+
+D:/git/Marlin4ST/stm32_cube/stm32F303RET/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_gpio.h:
+
+D:/git/Marlin4ST/stm32_cube/stm32F303RET/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_gpio_ex.h:
+
+D:/git/Marlin4ST/stm32_cube/stm32F303RET/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_dma.h:
+
+D:/git/Marlin4ST/stm32_cube/stm32F303RET/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_dma_ex.h:
+
+D:/git/Marlin4ST/stm32_cube/stm32F303RET/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_cortex.h:
+
+D:/git/Marlin4ST/stm32_cube/stm32F303RET/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_flash.h:
+
+D:/git/Marlin4ST/stm32_cube/stm32F303RET/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_flash_ex.h:
+
+D:/git/Marlin4ST/stm32_cube/stm32F303RET/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_i2c.h:
+
+D:/git/Marlin4ST/stm32_cube/stm32F303RET/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_i2c_ex.h:
+
+D:/git/Marlin4ST/stm32_cube/stm32F303RET/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_pwr.h:
+
+D:/git/Marlin4ST/stm32_cube/stm32F303RET/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_pwr_ex.h:
